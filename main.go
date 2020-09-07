cd9f37c964522690849cdd0f29cb911e0d10507a
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-09-07 19:42:59 +0000

    Read file

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, grep")
+	args := os.Args[1:]
+	if len(args) != 1 {
+		log.Fatal("missing filename arg")
+	}
+
+	fileName := args[0]
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal("cannot read file", err)
+	}
+	defer file.Close()
 }
