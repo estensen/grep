f4040d7681067641de6a112a404240eb369b1255
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-09-08 15:11:51 +0000

    Match lines with pattern

--- main.go
+++ main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	args := os.Args[1:]
-	if len(args) != 1 {
-		log.Fatal("missing filename arg")
+	if len(args) != 2 {
+		log.Fatal("usage: grep <pattern> <filename>")
 	}
 
-	fileName := args[0]
+	pattern := args[0]
+	fileName := args[1]
 
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal("cannot read file", err)
 	}
 	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, pattern) {
+			log.Println(line)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
